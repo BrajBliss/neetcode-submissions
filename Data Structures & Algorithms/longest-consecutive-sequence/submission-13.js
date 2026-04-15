13b9b0adaf285850e0663a315b35f9fe1b685764
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-04-15 23:59:58 +0530

    Add: longest-consecutive-sequence - submission-13

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-13.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let res = 0;
+        const set = new Set(nums);
+        for (const num of nums) {
+            if (!set.has(num - 1)) {
+                let length = 1;
+                while (set.has(num + length)) {
+                    length++;
+                }
+                res = Math.max(res, length)
+            }
+        }
+        return res;
+    }
+}
