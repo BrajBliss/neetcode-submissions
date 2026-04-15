27e7db189e8746739ea8148799b8bda2b16380ea
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-04-15 14:59:27 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        for (const each of s) {
+            if (each === '{') stack.push('}');
+            else if (each === '[') stack.push(']');
+            else if (each === '(') stack.push(')');
+            else if (!stack.length || each !== stack.pop()) return false;
+        }
+        return !stack.length;
+    }
+}
