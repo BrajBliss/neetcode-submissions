a4f8d2d51b38c20260a22dff6e326a6aa765adf5
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-04-15 17:26:06 +0530

    Add: two-integer-sum-ii - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-8.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        for (let i = 0; i < numbers.length; i++) {
+            let l = i + 1, r = numbers.length - 1;
+            const temp = target - numbers[i];
+            while (l <= r) {
+                const mid = l + Math.floor((r - l) / 2);
+                if (temp === numbers[mid]) {
+                    return [i+1, mid+1];
+                } else if (temp > numbers[mid]) {
+                    l = mid + 1;
+                } else r = mid - 1;
+            }
+        }
+        return [];
+    }
+}
