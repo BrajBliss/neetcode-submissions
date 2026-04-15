d28b3f19b15bd45a8310adad6912c89f178b6a82
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-04-15 19:52:07 +0530

    Add: top-k-elements-in-list - submission-16

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-16.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const count = {};
+        for (const num of nums) {
+            count[num] = 1 + (count[num] || 0);
+        }
+        const minHeap = new MinPriorityQueue((x) => x[1]);
+        for (const [num, freq] of Object.entries(count)) {
+            minHeap.enqueue([num, freq]);
+            if (minHeap.size() > k) minHeap.dequeue();
+        }
+        const res = [];
+        for (let i = 0; i < k; i++) {
+            const [num, freq] = minHeap.dequeue();
+            res.push(num)
+        }
+        return res;
+    }
+}
