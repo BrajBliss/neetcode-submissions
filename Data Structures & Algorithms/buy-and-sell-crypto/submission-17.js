2b567db0953a07168e90036558b23e5b03270720
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-04-27 22:35:01 +0530

    Add: buy-and-sell-crypto - submission-17

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-17.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let res = 0;
+        for (let i = 0; i < prices.length; i++) {
+            for (let j = i + 1; j < prices.length; j++) {
+                const buy = prices[i];
+                const sell = prices[j];
+                res = Math.max(res, sell - buy)
+            }
+        }
+        return res;
+    }
+}
