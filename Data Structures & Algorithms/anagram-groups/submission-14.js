6314f23a77456ac6d5908467217b1e29ac9d1198
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-04-27 23:03:31 +0530

    Add: anagram-groups - submission-14

---
+++ Data Structures & Algorithms/anagram-groups/submission-14.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const res = {};
+        for (const s of strs) {
+            const sorted = s.split('').sort().join('');
+            if (!res[sorted]) res[sorted] = [];
+            res[sorted].push(s);
+        }
+        return Object.values(res);
+    }
+}
