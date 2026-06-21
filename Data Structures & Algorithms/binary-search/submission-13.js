e614cf69fd119bc49022fe0f973f33c7d8b7c40c
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-06-21 22:13:31 +0530

    Add: binary-search - submission-13

---
+++ Data Structures & Algorithms/binary-search/submission-13.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left = 0, right = nums.length - 1;
+        while (left <= right) {
+            const mid = (left + right) >> 1;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
