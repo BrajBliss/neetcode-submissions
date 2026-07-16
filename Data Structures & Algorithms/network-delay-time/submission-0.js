d921823fb4af7f510473e7dd7283f172ef0f8986
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-07-16 07:48:15 +0530

    Add: network-delay-time - submission-0

---
+++ Data Structures & Algorithms/network-delay-time/submission-0.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[][]} times
+     * @param {number} n
+     * @param {number} k
+     * @return {number}
+     */
+    networkDelayTime(times, n, k) {
+        const graph = {};
+        for (let i = 1; i <= n; i++) {
+            graph[i] = [];
+        }
+        for (const [src, dest, weight] of times) {
+            graph[src].push([dest, weight]);
+        }
+        const distances = {};
+        for (let i = 1; i <= n; i++) {
+            distances[i] = Infinity;
+        }
+        distances[k] = 0;
+        const pq = new MinPriorityQueue((entry) => entry[1]);
+        pq.enqueue([k, 0]);
+        while (!pq.isEmpty()) {
+            const [ currentNode, currentDistance ] = pq.dequeue();
+            if (distances[currentNode] < currentDistance) {
+                continue;
+            }
+            for (const [neighbour, distanceToNeighbour] of graph[currentNode]) {
+                const newDistance = currentDistance + distanceToNeighbour;
+                if (newDistance < distances[neighbour]) {
+                    distances[neighbour] = newDistance;
+                    pq.enqueue([neighbour, newDistance]);
+                }
+            }
+        }
+        let maxTime = 0;
+            for (let i = 1; i <= n; i++) {
+                if (distances[i] === Infinity) {
+                    return -1;
+                }
+                maxTime = Math.max(maxTime, distances[i]);
+            }
+            return maxTime;
+    }
+}
