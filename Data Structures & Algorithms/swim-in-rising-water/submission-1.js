99b4892b0295e160e907622841706d1fc18dc4d8
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-07-19 04:11:17 +0530

    Add: swim-in-rising-water - submission-1

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-1.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    swimInWater(grid) {
+        const n = grid.length;
+        const visited = Array.from({length: n}, () => new Array(n).fill(false));
+        const pq = new MinPriorityQueue(entry => entry[0]);
+        // maxElevationSoFar, row, col
+        pq.enqueue([grid[0][0], 0, 0]);
+        visited[0][0] = true;
+        const directions = [
+            [-1, 0],
+            [1, 0],
+            [0, -1],
+            [0, 1]
+        ];
+        while (!pq.isEmpty()) {
+            const [currentMaxElevation, r, c] = pq.dequeue();
+            if (r === n - 1 && c === n - 1) {
+                return currentMaxElevation;
+            }
+            for (const [dr, dc] of directions) {
+                const newR = dr + r;
+                const newC = dc + c;
+                if (newR >= 0 && newC >= 0 && newR < n && newC < n && !visited[newR][newC]) {
+                    visited[newR][newC] = true;
+                    const nextMax = Math.max(currentMaxElevation, grid[newR][newC]);
+                    pq.enqueue([nextMax, newR, newC]);
+                }
+            }
+        }
+        return 0;
+    }
+}
