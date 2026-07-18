061c76d8c2737eece1df66f7a021fb758b485c80
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-07-19 01:37:09 +0530

    Add: cheapest-flight-path - submission-3

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-3.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} flights
+     * @param {number} src
+     * @param {number} dst
+     * @param {number} k
+     * @return {number}
+     */
+    findCheapestPrice(n, flights, src, dst, k) {
+        const graph = {};
+        for (let i = 0; i < n; i++) {
+            graph[i] = [];
+        }
+        for (const [src, dst, price] of flights) {
+            graph[src].push([dst, price]);
+        }
+        const minStops = new Array(n).fill(Infinity);
+        const pq = new MinPriorityQueue(entry => entry[0]);
+        pq.enqueue([0, src, 0]);
+        while (!pq.isEmpty()) {
+            const [currentCost, currentAirport, currentStops] = pq.dequeue();
+            if (currentAirport === dst) {
+                return currentCost;
+            }
+            if (currentStops > k) {
+                continue;
+            }
+            if (minStops[currentAirport] <= currentStops) {
+                continue;
+            }
+            minStops[currentAirport] = currentStops;
+            for (const [neighbour, price] of graph[currentAirport]) {
+                const newCost = price + currentCost;
+                pq.enqueue([newCost, neighbour, currentStops + 1]);
+            }
+        }
+        return -1;
+    }
+}
