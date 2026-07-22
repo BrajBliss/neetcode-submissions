7d563d9afa113dd5f0501e7186c755883761e385
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-07-22 08:48:07 +0530

    Add: longest-increasing-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    lengthOfLIS(nums) {
+        if (!nums || !nums.length) {
+            return 0;
+        }
+        const n = nums.length;
+        const dp = new Array(n).fill(1);
+        for (let i = 1; i < n; i++) {
+            for (let j = 0 ; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+        }
+        return Math.max(...dp);
+    }
+}
