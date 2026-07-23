4ed874c431989db57f177b28dbc39624c8e97a5d
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-07-23 09:30:21 +0530

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string[]} wordDict
+     * @return {boolean}
+     */
+    wordBreak(s, wordDict) {
+        const dp = new Array(s.length + 1).fill(false);
+        dp[0] = true;
+        for (let i = 1; i <= s.length; i++) {
+            for (const word of wordDict) {
+                if (i >= word.length && dp[i - word.length]) {
+                    if (s.slice(i - word.length, i) === word) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return dp[s.length];
+    }
+}
