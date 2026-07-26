240d69cbd44e81570be80a8a3f29a3829af7dadd
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-07-27 04:15:37 +0530

    Add: partition-labels - submission-4

---
+++ Data Structures & Algorithms/partition-labels/submission-4.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} S
+     * @return {number[]}
+     */
+    partitionLabels(S) {
+        const map = new Map();
+        const s = S;
+        const n = s.length;
+        for (let i = 0; i < n; i++) {
+            const char = s[i];
+            map.set(char, i);
+        }
+        const res = [];
+        let start = 0;
+        let end = 0;
+        for (let i = 0; i < n; i++) {
+            const char = s[i];
+            end = Math.max(end, map.get(char));
+            if (i === end) {
+                res.push(end - start + 1);
+                start = i + 1;
+            }
+        }
+        return res;
+    }
+}
