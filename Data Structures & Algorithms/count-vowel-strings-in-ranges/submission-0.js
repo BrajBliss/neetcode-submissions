d13085768d43dfe9858f082f1027ccd5327cd145
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-07-27 13:14:42 +0530

    Add: count-vowel-strings-in-ranges - submission-0

---
+++ Data Structures & Algorithms/count-vowel-strings-in-ranges/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @param {number[][]} queries
+     * @return {number[]}
+     */
+    fits(word) {
+        const vowels = ['a', 'e', 'i', 'o', 'u'];
+        if (vowels.includes(word[0]) && vowels.includes(word[word.length - 1])) {
+            return true;
+        }
+    }
+    vowelStrings(words, queries) {
+        const res = [];
+        for (const [l, r] of queries) {
+            let count = 0;
+            for (let i = l; i <= r; i++) {
+                if (!this.fits(words[i])) {
+                    continue;
+                } else count++;
+            }
+            res.push(count);
+        }
+        return res;
+    }
+}
