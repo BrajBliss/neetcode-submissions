eb162faa762c2ef5630b347752d1be3af62cf8f4
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-07-31 12:27:10 +0530

    Add: count-squares - submission-1

---
+++ Data Structures & Algorithms/count-squares/submission-1.js
@@ -0,0 +1,35 @@
+class CountSquares {
+    constructor() {
+        this.points = [];
+    }
+
+    /**
+     * @param {number[]} point
+     * @return {void}
+     */
+    add(point) {
+        this.points.push(point);
+    }
+
+    /**
+     * @param {number[]} point
+     * @return {number}
+     */
+    count(point) {
+        const [qx, qy] = point;
+        let totalSquares = 0;
+        for (let i = 0; i < this.points.length; i++) {
+            const [px, py] = this.points[i];
+            if (Math.abs(px - qx) === Math.abs(py - qy) && px !== qx) {
+                let countP1 = 0;
+                let countP2 = 0;
+                for (const [x, y] of this.points) {
+                    if (x === px && y === qy) countP1++;
+                    if (x === qx && y === py) countP2++;
+                }
+                totalSquares += countP1 * countP2;
+            }
+        }
+        return totalSquares;
+    }
+}
