0a96c082caebde91a84189602bbdf0dd0ae1a0b2
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-08-08 00:15:52 +0530

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isValid(node, left, right) {
+        if (!node) return true;
+        if (!(left < node.val && right > node.val)) {
+            return false;
+        }
+        return this.isValid(node.left, left, node.val) && this.isValid(node.right, node.val, right);
+    }
+    isValidBST(root) {
+        return this.isValid(root, -Infinity, Infinity);
+    }
+}
