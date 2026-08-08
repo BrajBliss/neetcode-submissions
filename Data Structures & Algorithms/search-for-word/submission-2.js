32cef1406061e3820c65fdecf02bdba8f51fe613
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-08-08 22:25:46 +0530

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.js
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @param {string} word
+     * @return {boolean}
+     */
+    exist(board, word) {
+        const rows = board.length;
+        const cols = board[0].length;
+        const visited = new Set();
+        function dfs(r, c, i) {
+            if (i === word.length) {
+                return true;
+            }
+            const key = `${r},${c}`;
+            if (
+                r < 0 ||
+                r >= rows ||
+                c < 0 ||
+                c >= cols ||
+                visited.has(key) ||
+                board[r][c] !== word[i]
+            )
+                return false;
+            visited.add(key);
+            const directions = [
+                [-1, 0],
+                [1, 0],
+                [0, -1],
+                [0, 1],
+            ];
+            for (const [dr, dc] of directions) {
+                const nr = r + dr;
+                const nc = c + dc;
+                if (dfs(nr, nc, i + 1)) {
+                    visited.delete(key);
+                    return true;
+                }
+            }
+            visited.delete(key);
+            return false;
+        }
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                if (dfs(r, c, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
