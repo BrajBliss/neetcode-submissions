adaadcc9b992ec56e913f5287c2bce2251ece5a2
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-08-18 01:57:29 +0530

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        for (const c of s) {
+            if (c === "(") stack.push(")");
+            else if (c === "[") stack.push("]");
+            else if (c === "{") stack.push("}");
+            else if (!stack.length || stack.pop() !== c) return false;
+        }
+        return !stack.length;
+    }
+}
