2f1f674a5a3f2ee9f88e5d2ca144b3f0fd2614ad
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-08-18 21:35:39 +0530

    Add: top-k-elements-in-list - submission-25

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-25.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        // const map = new Map();
+        // for (const num of nums) {
+        //     map.set(num, 1 + (map.get(num) || 0));
+        // }
+        // const arr = [...map];
+        // arr.sort((a, b) => b[1] - a[1]);
+        // return arr.slice(0, k).map((pair) => pair[0]);
+
+        const map = new Map();
+        for (const num of nums) {
+            map.set(num, 1 + (map.get(num) || 0));
+        }
+        const pq = new MinPriorityQueue((item) => item[1]);
+        map.forEach((freq, num) => {
+            pq.enqueue([num, freq]);
+            if (pq.size() > k) pq.dequeue();
+        });
+        const res = [];
+        for (let i = 0; i < k; i++) {
+            const [num, freq] = pq.dequeue();
+            res.push(num);
+        }
+        return res;
+    }
+}
