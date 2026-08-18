90936799f6338dcd0a7550a7d6840eea44021300
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-08-18 19:02:32 +0530

    Add: anagram-groups - submission-23

---
+++ Data Structures & Algorithms/anagram-groups/submission-23.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        // const map = new Map();
+        // for (const s of strs) {
+        //     const sortedS = s.split("").sort().join("");
+        //     if (!map.has(sortedS)) map.set(sortedS, []);
+        //     map.get(sortedS).push(s);
+        // }
+        // return Array.from(map.values());
+
+        const map = new Map();
+        return Array.from(map.values());
+    }
+}
