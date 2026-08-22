d9f52c37f9a9c224b75026b6c827bc5da4602525
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-08-22 08:11:58 +0530

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.js
@@ -0,0 +1,51 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        if (edges.length > n - 1) return false;
+        const graph = new Map();
+        for (const [u, v] of edges) {
+            if (!graph.has(u)) {
+                graph.set(u, []);
+            }
+            graph.get(u).push(v);
+            if (!graph.has(v)) {
+                graph.set(v, []);
+            }
+            graph.get(v).push(u);
+        }
+        const visited = new Set();
+        // function dfs(node, parent) {
+        //     if (visited.has(node)) {
+        //         return false;
+        //     }
+        //     visited.add(node);
+        //     for (const neighbour of graph.get(node)) {
+        //         if (neighbour === parent) {
+        //             continue;
+        //         }
+        //         if (!dfs(neighbour, node)) {
+        //             return false;
+        //         }
+        //     }
+        //     return true;
+        // }
+        // return dfs(0, -1) && visited.size === n;
+        const q = [[0, -1]];
+        let head = 0;
+        visited.add(0);
+        while (head < q.length) {
+            const [node, parent] = q[head++];
+            for (const neighbour of graph.get(node) || []) {
+                if (parent === neighbour) continue;
+                if (visited.has(neighbour)) return false;
+                visited.add(neighbour);
+                q.push([neighbour, node]);
+            }
+        }
+        return visited.size === n;
+    }
+}
