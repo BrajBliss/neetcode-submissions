0def97700f79fb1c4c19c14bfec5e677109b2d62
Amay <50140643+BrajBliss@users.noreply.github.com>
2026-09-14 23:04:14 +0530

    Add: merge-k-sorted-linked-lists - submission-6

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-6.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode[]} lists
+     * @return {ListNode}
+     */
+    mergeKLists(lists) {
+        // const nodes = [];
+        // for (let list of lists) {
+        //     while (list) {
+        //         nodes.push(list.val);
+        //         list = list.next;
+        //     }
+        // }
+        // nodes.sort((a, b) => a - b);
+        // const res = new ListNode(0);
+        // let curr = res;
+        // for (const node of nodes) {
+        //     curr.next = new ListNode(node);
+        //     curr = curr.next;
+        // }
+        // return res.next;
+
+        if (lists.length === 0) return null;
+        const minHeap = new MinPriorityQueue((item) => item.val);
+        for (const list of lists) {
+            if (list) {
+                minHeap.enqueue(list);
+            }
+        }
+        const dummy = new ListNode(0);
+        let curr = dummy;
+        while (minHeap.size()) {
+            let node = minHeap.dequeue();
+            curr.next = node;
+            curr = curr.next;
+            node = node.next;
+            if (node) minHeap.enqueue(node);
+        }
+        return dummy.next;
+    }
+}
